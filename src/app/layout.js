9faeb34f5de425858a1b6fb89ee790dd1f1ef7fc
Praveen Shinde <shindepraveen002@gmail.com>
2024-06-09 18:11:51 +0530

    Feat: Added Vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           position="top-right"
           reverseOrder={false}
         />
-        <Analytics />
+        <Analytics/>
         <div className="container px-8  lg:px-20 xl:px-80 py-8 relative flex flex-col gap-4">
           <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:14px_14px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
           <Header />
